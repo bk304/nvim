909e09bf60eed614248af822236ba01776d571b8
bk304 <xnep1@hotmail.com>
2025-04-08 04:31:22 -0300

    nvim fix

--- lua/options.lua
+++ lua/options.lua
@@ -4,3 +4,20 @@ require "nvchad.options"
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+
+local opt = vim.opt
+
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.softtabstop = 4
+opt.expandtab = true -- space isntead of tab
+opt.autoindent = true
+opt.clipboard:append('unnamedplus') -- use system's clipboard
+
+vim.cmd [[
+  autocmd BufNewFile,BufRead *.y set filetype=c
+]]
+
+require("nvim-tree").setup({
+  respect_buf_cwd = true,
+})
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,25 +1,70 @@
 return {
-  {
-    "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
-    opts = require "configs.conform",
-  },
+    {
+        "stevearc/conform.nvim",
+        -- event = 'BufWritePre', -- uncomment for format on save
+        opts = require "configs.conform",
+    },
 
-  -- These are some examples, uncomment them if you want to see them work!
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require "configs.lspconfig"
-    end,
-  },
+    { "nvchad/volt",  lazy = true },
+    { "nvchad/menu",  lazy = true },
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+    {
+        'chipsenkbeil/distant.nvim',
+        lazy = false,
+        branch = 'v0.3',
+        config = function()
+            require('distant'):setup()
+        end
+    },
+
+
+    {
+        "tris203/precognition.nvim",
+        --event = "VeryLazy",
+        lazy = false,
+        opts = {
+        -- startVisible = true,
+        -- showBlankVirtLine = true,
+        -- highlightColor = { link = "Comment" },
+        -- hints = {
+        --      Caret = { text = "^", prio = 2 },
+        --      Dollar = { text = "$", prio = 1 },
+        --      MatchingPair = { text = "%", prio = 5 },
+        --      Zero = { text = "0", prio = 1 },
+        --      w = { text = "w", prio = 10 },
+        --      b = { text = "b", prio = 9 },
+        --      e = { text = "e", prio = 8 },
+        --      W = { text = "W", prio = 7 },
+        --      B = { text = "B", prio = 6 },
+        --      E = { text = "E", prio = 5 },
+        -- },
+        -- gutterHints = {
+        --     G = { text = "G", prio = 10 },
+        --     gg = { text = "gg", prio = 9 },
+        --     PrevParagraph = { text = "{", prio = 8 },
+        --     NextParagraph = { text = "}", prio = 8 },
+        -- },
+        -- disabled_fts = {
+        --     "startify",
+        -- },
+        },
+    },
+
+    -- These are some examples, uncomment them if you want to see them work!
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            require "configs.lspconfig"
+        end,
+    },
+
+    -- {
+    -- 	"nvim-treesitter/nvim-treesitter",
+    -- 	opts = {
+    -- 		ensure_installed = {
+    -- 			"vim", "lua", "vimdoc",
+    --      "html", "css"
+    -- 		},
+    -- 	},
+    -- },
 }
